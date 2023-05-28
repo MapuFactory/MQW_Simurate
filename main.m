0cd8fe7b1b1c17f9f66ad137e09ec337b1233387
MapuFactory <yusugiyamamapu@gmail.com>
2023-05-28 17:19:45 +0900

    ｌｋｎ

--- main.m
+++ main.m
@@ -1,106 +1,106 @@
-#define SETFILE "set.dat"
+SETFILE "set.dat"
 
-#define PI          3.1415926      /* PI                              */
-#define PIS         9.8696044      /* PI^2                            */
-//#define ML 1e-10
-#define ML          0.31e-9        /* [m]   [ML]→[m]                 */
-//#define ML          0.155e-9        /* [m]   [ML]→[m]                 */
-//#define ML          0.2715e-9        /* [m]   [ML]→[m]                 */
-#define MSTAR       9.109e-31      /* [kg]                            */
-#define ELEC        1.602e-19      /* [C]   Electron charge           */
-#define DIEELECSTAR 8.854e-12      /* [F/m] Vacum Dielectric Constant */
-#define HBAR        1.054e-34      /* [Js]                            */
-#define HH	6.626e-34		/*[Js]								*/
-#define KB          1.38e-23       /* [J/K]                           */
-#define TEMP        3.0e+2            /* [K]                             */
-#define KT          (KB*TEMP)
-//#define III (ELEC*ELEC*MSTAR*KB*TEMP/2/PIS/HBAR/HBAR/HBAR)
+PI          3.1415926      /* PI                              */
+PIS         9.8696044      /* PI^2                            */
+// ML 1e-10
+ ML          0.31e-9        /* [m]   [ML]→[m]                 */
+// ML          0.155e-9        /* [m]   [ML]→[m]                 */
+// ML          0.2715e-9        /* [m]   [ML]→[m]                 */
+ MSTAR       9.109e-31      /* [kg]                            */
+ ELEC        1.602e-19      /* [C]   Electron charge           */
+ DIEELECSTAR 8.854e-12      /* [F/m] Vacum Dielectric Constant */
+ HBAR        1.054e-34      /* [Js]                            */
+ HH	6.626e-34		/*[Js]								*/
+ KB          1.38e-23       /* [J/K]                           */
+ TEMP        3.0e+2            /* [K]                             */
+ KT          (KB*TEMP)
+// III (ELEC*ELEC*MSTAR*KB*TEMP/2/PIS/HBAR/HBAR/HBAR)
 /* 1.08872e12*/
-#define II (ELEC*ELEC*MSTAR*KB/2/PIS/HBAR/HBAR/HBAR)
-#define VI          0.6            /* 電流計算時の積分範囲 */
-#define DELTA       1e-5
-#define DELTAE      1e-9
-#define DX          10				/* 分割間隔 = ML/DX   DXが大きいと分割間隔が細かい */
-#define OUTPUT      1             /* ポテンシャルの出力の有無                        */
+ II (ELEC*ELEC*MSTAR*KB/2/PIS/HBAR/HBAR/HBAR)
+ VI          0.6            /* 電流計算時の積分範囲 */
+ DELTA       1e-5
+ DELTAE      1e-9
+ DX          10				/* 分割間隔 = ML/DX   DXが大きいと分割間隔が細かい */
+ OUTPUT      1             /* ポテンシャルの出力の有無                        */
 
-#define NI  1.45e16                   /* [/m3] */
-#define EG_SI  1.12                     /* [eV]   */
+ NI  1.45e16                   /* [/m3] */
+ EG_SI  1.12                     /* [eV]   */
 
-#define BASE 4.05
-#define pBASE (-5.17)
+ BASE 4.05
+ pBASE (-5.17)
 
-#define DIE_CAF2  6.76
-#define EAFF_CAF2 (4.05-1.0) /* 2ML物性値 Delta Ec = 1.0 [eV] 適用 */
-#define BAR_CAF2  -(EAFF_CAF2-BASE)
-#define MASS_CAF2 1
+ DIE_CAF2  6.76
+ EAFF_CAF2 (4.05-1.0) /* 2ML物性値 Delta Ec = 1.0 [eV] 適用 */
+ BAR_CAF2  -(EAFF_CAF2-BASE)
+ MASS_CAF2 1
 
-#define DIE_CDF2  8.83
-#define EAFF_CDF2 (4.05+0.6)
-#define BAR_CDF2  -(EAFF_CDF2-BASE)
-#define MASS_CDF2 0.4
+ DIE_CDF2  8.83
+ EAFF_CDF2 (4.05+0.6)
+ BAR_CDF2  -(EAFF_CDF2-BASE)
+ MASS_CDF2 0.4
 
-#define DIE_iSI   (11.8)
-#define EAFF_iSI  4.05
-#define BAR_iSI   -(EAFF_iSI-BASE)
-#define MASS_iSI  0.26
-#define NUMVALLY_SI 6
-#define MASS_SI_Z  0.98
-#define MASS_SI_XY 0.19
+ DIE_iSI   (11.8)
+ EAFF_iSI  4.05
+ BAR_iSI   -(EAFF_iSI-BASE)
+ MASS_iSI  0.26
+ NUMVALLY_SI 6
+ MASS_SI_Z  0.98
+ MASS_SI_XY 0.19
 
-#define DIE_AL    0
-#define EAFF_AL  15.83
-#define BAR_AL   -(EAFF_AL-BASE)
-#define MASS_AL  1
-#define EF_AL    (11.63)
+ DIE_AL    0
+ EAFF_AL  15.83
+ BAR_AL   -(EAFF_AL-BASE)
+ MASS_AL  1
+ EF_AL    (11.63)
 
-#define DIE_AU    0
-#define EAFF_AU  10.33
-#define BAR_AU   -(EAFF_AU-BASE)
-#define MASS_AU  1
-#define EF_AU    5.51
+ DIE_AU    0
+ EAFF_AU  10.33
+ BAR_AU   -(EAFF_AU-BASE)
+ MASS_AU  1
+ EF_AU    5.51
 
-#define DIE_pSI   (11.8)
-#define EAFF_pSI  -(4.05+1.12)
-#define BAR_pSI   -(EAFF_pSI-pBASE)
-#define MASS_pSI  0.55
+ DIE_pSI   (11.8)
+ EAFF_pSI  -(4.05+1.12)
+ BAR_pSI   -(EAFF_pSI-pBASE)
+ MASS_pSI  0.55
 
-#define DIE_pAL    0
-#define EAFF_pAL   0
-#define BAR_pAL   -(EAFF_pAL-pBASE)
-#define MASS_pAL  1
-#define EF_pAL    4.37
+ DIE_pAL    0
+ EAFF_pAL   0
+ BAR_pAL   -(EAFF_pAL-pBASE)
+ MASS_pAL  1
+ EF_pAL    4.37
 
-#define DIE_pCAF2  6.76
-#define EAFF_pCAF2 -(4.05+1.12+1)
-#define BAR_pCAF2  -(EAFF_pCAF2-pBASE)
-#define MASS_pCAF2 1
+ DIE_pCAF2  6.76
+ EAFF_pCAF2 -(4.05+1.12+1)
+ BAR_pCAF2  -(EAFF_pCAF2-pBASE)
+ MASS_pCAF2 1
 
-#define DIE_SIO2   3.9
-#define EAFF_SIO2  (4.05-2.9)
-#define BAR_SIO2   -(EAFF_SIO2-BASE)
-#define MASS_SIO2  0.26
-#define NUMVALLY_SI 6
-#define NUMVALLY_SI_Z 2
-#define NUMVALLY_SI_XY 4
+ DIE_SIO2   3.9
+ EAFF_SIO2  (4.05-2.9)
+ BAR_SIO2   -(EAFF_SIO2-BASE)
+ MASS_SIO2  0.26
+ NUMVALLY_SI 6
+ NUMVALLY_SI_Z 2
+ NUMVALLY_SI_XY 4
 
-//#define DIE_CoSi2   8.5
-//#define EAFF_CoSi2  17.7
-//#define BAR_CoSi2   -(EAFF_CoSi2-BASE)
-//#define MASS_CoSi2  1
-//#define EF_CoSi2    (13.0)
+// DIE_CoSi2   8.5
+// EAFF_CoSi2  17.7
+// BAR_CoSi2   -(EAFF_CoSi2-BASE)
+// MASS_CoSi2  1
+// EF_CoSi2    (13.0)
 
-#define LBAR	2
-#define WELL	(LBAR+1)
-#define RBAR	(LBAR+2)
-#define DIM		2
-#define LAYER	100 //利用する材料の層数の最大数．(ML数ではない)
-#define DIV     10
-#define EMAX	10
+ LBAR	2
+ WELL	(LBAR+1)
+ RBAR	(LBAR+2)
+ DIM		2
+ LAYER	100 //利用する材料の層数の最大数．(ML数ではない)
+ DIV     10
+ EMAX	10
 
-#define AAA	0
+ AAA	0
 
-#define RTD 1
-#define ALL 0
+ RTD 1
+ ALL 0
 char *name[LAYER];
 int N[2],layer,smt[LAYER],divnum[LAYER],NX[LAYER],valley[LAYER],cond[LAYER];
 double d[LAYER],die[LAYER],bar[LAYER],mass[LAYER],Ef[LAYER],Q[LAYER],Work[LAYER],massxy[LAYER];
